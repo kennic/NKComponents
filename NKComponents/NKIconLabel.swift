15644d8a167b0305514ef32b337348e75063f12a
Nam Kennic <namkennic@me.com>
2020-11-29 14:49:19 +0700

    NKIconLabel more details imageAlignment

--- NKComponents/NKIconLabel.swift
+++ NKComponents/NKIconLabel.swift
@@ -8,9 +8,23 @@
 
 import UIKit
 
-public enum NKIconLabelImageAlignment {
+public enum NKIconVerticalAlignment {
+	case top
+	case center
+	case bottom
+}
+
+public enum NKIconHorizontalAlignment {
 	case left
+	case center
 	case right
+}
+
+public enum NKIconLabelImageAlignment {
+	case left(verticalAlignment: NKIconVerticalAlignment = .center)
+	case right(verticalAlignment: NKIconVerticalAlignment = .center)
+	case top(horizontalAlign: NKIconHorizontalAlignment = .center)
+	case bottom(horizontalAlign: NKIconHorizontalAlignment = .center)
 }
 
 open class NKIconLabel: UILabel {
@@ -33,14 +47,14 @@ open class NKIconLabel: UILabel {
 		}
 	}
 	
-	public var imageAlignment: NKIconLabelImageAlignment = .left {
+	public var imageAlignment: NKIconLabelImageAlignment = .left(verticalAlignment: .center) {
 		didSet {
 			setNeedsDisplay()
 			setNeedsLayout()
 		}
 	}
 	
-	public var maxImageSize: CGSize = CGSize(width: 32, height: 32)
+	public var maxImageSize: CGSize = .zero
 	
 	public var extendSize: CGSize = .zero
 	public var edgeInsets: UIEdgeInsets = .zero {
@@ -67,8 +81,17 @@ open class NKIconLabel: UILabel {
 			return
 		}
 		
-		let textInsets = imageAlignment == .left ? 	UIEdgeInsets(top: edgeInsets.top, left: edgeInsets.left + min(iconSize.width, maxImageSize.width) + spacing, bottom: edgeInsets.bottom, right: edgeInsets.right) :
-													UIEdgeInsets(top: edgeInsets.top, left: edgeInsets.left, bottom: edgeInsets.bottom, right: edgeInsets.right + min(iconSize.width, maxImageSize.width) + spacing)
+		var textInsets: UIEdgeInsets = .zero
+		let maxWidth = maxImageSize.width > 0 ? min(iconSize.width, maxImageSize.width) : iconSize.width
+		let maxHeight = maxImageSize.height > 0 ? min(iconSize.height, maxImageSize.height) : iconSize.height
+		
+		switch imageAlignment {
+			case .left(_): textInsets = UIEdgeInsets(top: edgeInsets.top, left: edgeInsets.left + maxWidth + spacing, bottom: edgeInsets.bottom, right: edgeInsets.right)
+			case .right(_): textInsets = UIEdgeInsets(top: edgeInsets.top, left: edgeInsets.left, bottom: edgeInsets.bottom, right: edgeInsets.right + maxWidth + spacing)
+			case .top(_): textInsets = UIEdgeInsets(top: edgeInsets.top + maxHeight + spacing, left: edgeInsets.left, bottom: edgeInsets.bottom, right: edgeInsets.right)
+			case .bottom(_): textInsets = UIEdgeInsets(top: edgeInsets.top, left: edgeInsets.left, bottom: edgeInsets.bottom + maxHeight + spacing, right: edgeInsets.right)
+		}
+		
 		let textRect = bounds.inset(by: textInsets)
 		super.drawText(in: textRect)
 	}
@@ -77,15 +100,30 @@ open class NKIconLabel: UILabel {
 		super.layoutSubviews()
 		
 		guard let iconSize = imageView.image?.size else { return }
-		let imageMaxSize = CGSize(width: min(iconSize.width, maxImageSize.width), height: min(iconSize.height, maxImageSize.height))
+		let maxWidth = maxImageSize.width > 0 ? min(iconSize.width, maxImageSize.width) : iconSize.width
+		let maxHeight = maxImageSize.height > 0 ? min(iconSize.height, maxImageSize.height) : iconSize.height
 		let viewSize = bounds.size
 		
-		if imageAlignment == .left {
-			imageView.frame = CGRect(x: edgeInsets.left, y: (viewSize.height - imageMaxSize.height)/2, width: imageMaxSize.width, height: imageMaxSize.height)
+		var imageFrame: CGRect
+		switch imageAlignment {
+			case .left(let verticalAlignment):
+				let frameY = verticalAlignment == .top ? edgeInsets.top : verticalAlignment == .bottom ? (viewSize.height - maxHeight) : (viewSize.height - maxHeight)/2
+				imageFrame = CGRect(x: edgeInsets.left, y: frameY, width: maxWidth, height: maxHeight)
+			
+			case .right(let verticalAlignment):
+				let frameY = verticalAlignment == .top ? edgeInsets.top : verticalAlignment == .bottom ? (viewSize.height - maxHeight) : (viewSize.height - maxHeight)/2
+				imageFrame = CGRect(x: viewSize.width - maxWidth - edgeInsets.right, y: frameY, width: maxWidth, height: maxHeight)
+			
+			case .top(let horizontalAlignment):
+				let frameX = horizontalAlignment == .left ? edgeInsets.left : horizontalAlignment == .right ? (viewSize.width - maxWidth) : (viewSize.width - maxWidth)/2
+				imageFrame = CGRect(x: frameX, y: edgeInsets.top, width: maxWidth, height: maxHeight)
+			
+			case .bottom(let horizontalAlignment):
+				let frameX = horizontalAlignment == .left ? edgeInsets.left : horizontalAlignment == .right ? (viewSize.width - maxWidth) : (viewSize.width - maxWidth)/2
+				imageFrame = CGRect(x: frameX, y: viewSize.height - maxHeight - edgeInsets.bottom, width: maxWidth, height: maxHeight)
 		}
-		else {
-			imageView.frame = CGRect(x: viewSize.width - imageMaxSize.width - edgeInsets.right, y: (viewSize.height - imageMaxSize.height)/2, width: imageMaxSize.width, height: imageMaxSize.height)
-		}
+		
+		imageView.frame = imageFrame
 	}
 	
 	override open func sizeThatFits(_ size: CGSize) -> CGSize {
@@ -93,8 +131,18 @@ open class NKIconLabel: UILabel {
         let iconSize = imageView.image?.size ?? .zero
         let spacingValue = iconSize == .zero ? 0.0 : spacing
 
-		let imageMaxSize = CGSize(width: min(iconSize.width, maxImageSize.width), height: min(iconSize.height, maxImageSize.height))
-		return CGSize(width: superValue.width + spacingValue + imageMaxSize.width + edgeInsets.left + edgeInsets.right + extendSize.width, height: max(imageMaxSize.height, superValue.height) + edgeInsets.top + edgeInsets.bottom + extendSize.height)
+		let imageWidth = maxImageSize.width > 0 ? min(iconSize.width, maxImageSize.width) : iconSize.width
+		let imageHeight = maxImageSize.height > 0 ? min(iconSize.height, maxImageSize.height) : iconSize.height
+		
+		var result: CGSize
+		switch imageAlignment {
+			case .left(_), .right(_): result = CGSize(width: superValue.width + spacingValue + imageWidth, height: max(imageHeight, superValue.height))
+			case .top(_), .bottom(_): result = CGSize(width: max(imageWidth, superValue.width), height: superValue.height + spacingValue + imageHeight)
+		}
+		
+		result.width += edgeInsets.left + edgeInsets.right + extendSize.width
+		result.height += edgeInsets.top + edgeInsets.bottom + extendSize.height
+		return result
 	}
 	
 }
